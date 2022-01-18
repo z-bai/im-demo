098a338d70214ccde548dbc924f8efa5f2c6991a
Avery Bai <averyby@yahoo.com>
2022-01-19 00:15:24 +0800

    first commit

---
+++ src/add-image.js
@@ -0,0 +1,13 @@
+import Kiwi from './kiwi.png';
+import altText from './altText.txt';
+
+function addImage() {
+  const img = document.createElement('img');
+  img.alt = altText;
+  img.width = 300;
+  img.src = Kiwi;
+  const body = document.querySelector('body');
+  body.appendChild(img);
+}
+
+export default addImage;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024 // 3 kilobytes
+          }
+        }
+      },
+      {
+        test: /\.txt$/,
+        type: 'asset/source'
+      }
+    ]
+  }
+};
